4ff357c8d08f0ab2bd5f9bdf1e16cdd96e701f89
MohammedAlshami <alshamicourses@gmail.com>
2024-05-06 23:54:34 +0800

    a

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,3 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route('/dummy_response', methods=['POST',"GET"])
+def dummy_response():
+    data = request.get_json()
+    message = data.get('message')
+
+    # Modified dummy response JSON
+    response = {
+        "body": {
+            "message": "This is a dummy response from the bot."
+        }
+    }
+
+    return jsonify(response)
